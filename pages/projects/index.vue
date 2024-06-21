79547b6b3523aab7515cb99ed32637f065f18198
Axuata <axuata@gmail.com>
2024-06-21 19:22:12 +0900

    add: "プロジェクト"ページに「Wiki関係」セクションを追加 add: "プロジェクト"ページの「Wiki関係」セクションにBernic Studio Wikiを追加

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -7,4 +7,7 @@
   <GlobalSectionWithSlot titleText="ウェブ関係">
     <GlobalCard titleText="Bernic Studio（ウェブサイト）" descriptionTextHtml="<p>Bernic Studioのホームページ。<br><a href='https://github.com/bernic-studio/bernic-studio'>Github</a></p>" />
   </GlobalSectionWithSlot>
+  <GlobalSectionWithSlot titleText="Wiki関係">
+    <GlobalCard titleText="Bernic Studio Wiki" descriptionTextHtml="<p>Bernic StudioのWiki。<br><a href='https://bernic-studio.notion.site/b712c50a26094da7acb45364b0fa0fb4?pvs=4'>Notion</a></p>" />
+  </GlobalSectionWithSlot>
 </template>
