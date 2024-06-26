ea20614c50048499b653a9d976a928b19e8c8622
Axuata <axuata@gmail.com>
2024-06-26 13:39:33 +0900

    add:「プロジェクト」ページの「ソフトウェア」セクションに「Note」を追加

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -11,6 +11,9 @@
   <GlobalSectionWithSlot titleText="ウェブ関係">
     <GlobalCard titleText="Bernomix（ウェブサイト）" descriptionTextHtml="<p>Bernomixのホームページ。<br><a href='https://github.com/bernomix/bernomix'>Github</a></p>" />
   </GlobalSectionWithSlot>
+  <GlobalSectionWithSlot titleText="ソフトウェア">
+    <GlobalCard titleText="Note" descriptionTextHtml="<p>モダンなUIのメモ帳<br><a href='https://github.com/bernomix/note'>Github</a></p>" />
+  </GlobalSectionWithSlot>
   <GlobalSectionWithSlot titleText="Wiki関係">
     <GlobalCard titleText="Bernomix Wiki" descriptionTextHtml="<p>BernomixのWiki。<br><a href='https://bernomix.notion.site/Bernomix-Wiki-73cd87a7f6a74d11900bba37e83e9c01?pvs=4'>Notion</a></p>" />
   </GlobalSectionWithSlot>
